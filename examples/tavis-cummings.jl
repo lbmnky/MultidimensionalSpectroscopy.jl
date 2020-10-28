76ad0b21bceb353ec57e5b7949518933de0fdd85
lbmnky <lars.h.mewes@gmail.com>
2020-10-28 23:06:59 +0100

    first try with several atoms in cavity

--- examples/tavis-cummings.jl
+++ examples/tavis-cummings.jl
@@ -19,3 +19,50 @@ pygui(true)
 calc_2d = true
 
 cmp = cmds.create_colormap("bright");
+
+
+
+
+b = NLevelBasis(2)
+H = diagonaloperator(b, [0, 1])
+j12 = transition(b, 1, 2) # decay from 2nd into 1st level
+
+Nb = 2
+b_mb = ManyBodyBasis(b, bosonstates(b, Nb))
+
+H_mb = manybodyoperator(b_mb, H)
+
+j12_mb = manybodyoperator(b_mb, j12)
+
+
+wc = 1
+N = 1               # number of cavity fock states
+b_fock = FockBasis(N)
+a = destroy(b_fock) ⊗ one(b_mb)
+at = dagger(a)
+
+wa = 1
+sm = one(b_fock) ⊗ j12_mb
+sp = dagger(sm)
+println(sm)
+
+Psi0 = fockstate(b_fock,0) ⊗ basisstate(b_mb,[Nb, 0])    # start with an excited cavity
+rho0 = dm(Psi0)
+
+g = 0.24
+
+H_full = wc * at * a + wa * sp * sm + g * (at * sm + a * sp)
+
+Γ = [0.9]
+J_mb = [sm]
+
+tlist = [0:0.1:50;]
+
+tout, rhot = timeevolution.master(tlist,Psi0,H_full,J_mb)
+
+nc = real(expect(at * a, rhot))
+na = real(expect(sp * sm, rhot))
+
+figure(figsize=(6,3))
+plot(tout,nc)
+plot(tout,na)
