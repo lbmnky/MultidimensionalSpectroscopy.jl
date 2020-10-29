295c2f74c0a17bc26cecaca8dd1947fe9f220c5e
lbmnky <lars.h.mewes@gmail.com>
2020-10-29 08:52:35 +0100

    on the cloud

--- examples/tavis-cummings.jl
+++ examples/tavis-cummings.jl
@@ -35,30 +35,33 @@ H_mb = manybodyoperator(b_mb, H)
 j12_mb = manybodyoperator(b_mb, j12)
 
 
-wc = 1
+wc = 0.9
 N = 1               # number of cavity fock states
 b_fock = FockBasis(N)
 a = destroy(b_fock) ⊗ one(b_mb)
 at = dagger(a)
 
-wa = 1
+wa = 1.1
 sm = one(b_fock) ⊗ j12_mb
 sp = dagger(sm)
 println(sm)
 
-Psi0 = fockstate(b_fock,0) ⊗ basisstate(b_mb,[Nb, 0])    # start with an excited cavity
+Psi0 = fockstate(b_fock,0) ⊗ basisstate(b_mb,[Nb,0])    # start with an excited cavity
 rho0 = dm(Psi0)
 
+rho0 = rho0 * a
 g = 0.24
+
+### this should be on the cloud
 
 H_full = wc * at * a + wa * sp * sm + g * (at * sm + a * sp)
 
-Γ = [0.9]
+Γ = [0.01]
 J_mb = [sm]
 
 tlist = [0:0.1:50;]
 
-tout, rhot = timeevolution.master(tlist,Psi0,H_full,J_mb)
+tout, rhot = timeevolution.master(tlist,rho0,H_full,J_mb;rates=Γ)
 
 nc = real(expect(at * a, rhot))
 na = real(expect(sp * sm, rhot))
